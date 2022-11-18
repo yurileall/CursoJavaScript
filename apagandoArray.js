ee31cce4ff3f85bbf6bbf1fb7185b9ffc226e017
Yuri ylc. Leal Cerqueira <yurilc@transalvadornet.pms.ad>
2022-11-18 08:34:05 -0300

    primeiro commit

---
+++ apagandoArray.js
@@ -0,0 +1,9 @@
+let numeros = [1,2,3,4,5];
+let outros = numeros;
+
+// Apagando o Array
+numeros.length = 0;
+
+console.log(numeros);
+
+
---
+++ combinandoArray.js
@@ -0,0 +1,5 @@
+const nome = ['Yuri'];
+const sobrenome = ['Leal'];
+
+const nomeCompleto = nome.concat(sobrenome);
+console.log(nomeCompleto);
